98f69a9239060147162f175bd9db40fc8d1dd221
Jake Anderson <anderson.jake.scott@gmail.com>
2018-01-24 10:17:38 -0700

    hello world program to play around with

---
+++ HelloWorld/platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/main.m \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/jakeanderson/GitHub/practice-nativescript/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
